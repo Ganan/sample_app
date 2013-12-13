c9b5fc445a84abbf5c953771085f2b561061672c
Ganan <choganan@hotmail.co.jp>
2013-12-13 22:59:49 +0900

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,5 +1,49 @@
 require 'spec_helper'
 
+#=begin
+describe "Static pages" do
+	let(:base_title){"Ruby on Rails Tutorial Sample App"}
+	#base_title = "Ruby on Rails Tutorial Smple App"          この書き方はだめ。この中はrubyじゃない
+
+	describe "Home page" do
+    	it "should have the content 'Sample App'" do
+    		visit '/static_pages/home'
+      		expect(page).to have_content('Sample App')
+    	end
+    	it "should have the title" do
+    		visit '/static_pages/home'
+    		expect(page).to have_title("#{base_title}")
+    	end
+    	it "should not have a custom page title" do
+    		visit '/static_pages/home'
+    		expect(page).not_to have_title('| Home')
+    	end
+  	end
+
+	describe "Help page" do
+		it "should have the content 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_content('Help')
+		end
+		it "should have the title 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_title("#{base_title} | Help")
+		end
+	end
+
+	describe "About page" do
+		it "should have the content 'About Us'" do
+			visit '/static_pages/about'
+			expect(page).to have_content('About Us')
+		end
+		it "should have the title 'About Us'" do
+			visit '/static_pages/about'
+			expect(page).to have_title("#{base_title} | About Us")
+		end
+	end
+end
+#=end
+=begin
 describe "Static pages" do
 	describe "Home page" do
     	it "should have the content 'Sample App'" do
@@ -8,8 +52,13 @@ describe "Static pages" do
     	end
     	it "should have the title 'Home'" do
     		visit '/static_pages/home'
-    		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    		expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     	end
+    	it "should not have a custom page title" do
+    		visit '/static_pages/home'
+    		expect(page).not_to have_title('| Home')
+    	end
+
   	end
 
 	describe "Help page" do
@@ -34,3 +83,5 @@ describe "Static pages" do
 		end
 	end
 end
+=end
+
